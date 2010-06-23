e7df762d3d3f608a5b354d69ba170ec62dea1386
scottmessinger <scottmessinger@gmail.com>
2010-06-23 00:29:08 -0400

    Tidied up the maryland.rb file and added some versions to our gems.

--- app/models/maryland.rb
+++ app/models/maryland.rb
@@ -1,18 +1,18 @@
 class Maryland
   include MongoMapper::Document
   
-  key :TYPE, String
-  key :LABEL, String
-  key :STANDARD, String
-  key :TOPIC, String
-  key :INDICATOR, String
-  key :OBJECTIVE, String
-  key :ASSESSMENT_LIMIT, String
+  key :type, String
+  key :label, String
+  key :standard, String
+  key :topic, String
+  key :indicator, String
+  key :objective, String
+  key :assessment_limit, String
   
   key :related_cc_array, Array
   
   key :perfect_alignment, Boolean #1 = true, 0=false
   key :common_core_alignment_note, String
   
-  many :common_cores :in => :related_cc_array
+  # many :common_cores, :in => :related_cc_array
 end
